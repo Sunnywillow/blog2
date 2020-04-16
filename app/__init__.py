58bcdb0fc6977226dedef970fb3ccad9230078d4
Sunnywillow <Joseph3588.me@gmail.com>
2020-04-16 11:12:19 +0800

    准备cherry-pick

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
@@ -14,3 +15,33 @@ def create_app（config_name):  # 工厂函数
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
+=======
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
+
+>>>>>>> fb13ded... Chapter 7: Large file structure (7a)
