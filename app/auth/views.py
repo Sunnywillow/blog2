8a2b855d337837997baac7af45fba59f571b4d6b
Sunnywillow <Joseph3588.me@gmail.com>
2020-04-20 17:34:23 +0800

    注册功能

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, request, url_for, flash, redirect
 from flask_login import login_user, logout_user, login_required
 from . import auth
+from .. import db
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -21,6 +22,20 @@ def login():
                 next =url_for('main.index')
                 # 重定向到首页
     return render_template('auth/login.html', form=form)  #  都存在得定向到登录页面
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm() # 实例化form
+    if form.validate_on_submit(): # 如果点击提交
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))  # 跳转到登陆页面
+    return render_template('auth/register.html', form=form)
 
 
 @auth.route('/logout')
