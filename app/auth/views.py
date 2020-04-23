16c37e1687dfdfa42c13bbad68ce00d5f06c2344
Sunnywillow <Joseph3588.me@gmail.com>
2020-04-23 19:59:29 +0800

    第八章 用户身份验证完成

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,25 @@
 from flask import render_template, request, url_for, flash, redirect
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from .. import db
 from ..models import User
 from .forms import LoginForm, RegistrationForm
+from ..email import send_email
+
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+        and not current_user.confirmed \
+        and request.blueprint != 'auth' \
+        and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -21,7 +37,9 @@ def login():
             # 如果 next 不存在 或者 next以/开始
                 next =url_for('main.index')
                 # 重定向到首页
-    return render_template('auth/login.html', form=form)  #  都存在得定向到登录页面
+            return redirect (next)
+        flash('Invalid username or password.')
+    return render_template('auth/login.html', form=form)  #  都存在问题重新渲染登录页面
 
 
 @auth.route('/register', methods=['GET', 'POST'])
@@ -33,9 +51,32 @@ def register():
                     password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('You can now login.')
-        return redirect(url_for('auth.login'))  # 跳转到登陆页面
+        # 当前用户生成token
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm Your Account',
+                   'auth/email/confirm', user=user, toekn=token)
+        flash('A confirmation email has been sent to you by email.')
+        return redirect(url_for('main.index'))
     return render_template('auth/register.html', form=form)
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    # 检验当前用户是否已经确认过
+    if current_user.confirmed:
+        # 确认过则重定向到首页
+        return redirect(url_for('main.index'))  # 重定向主页
+    # 未确认过则再次进行确认token,如果token有效
+    if current_user.confirm(token):
+        # 提交到数据库, 创建id
+        db.session.commit()
+        # 反馈信息
+        flash('You have confirmed your account. Thanks!')
+    # token无效则返回
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
 
 
 @auth.route('/logout')
@@ -43,4 +84,14 @@ def register():
 def logout():
     logout_user()
     flash('You have been logged out.')
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():  # 再次发送确认消息
+    token = current_user.generate_confirmation_token()  # 当前用户生成toekn
+    send_email(current_user.email, 'Confirm Your Account',
+               'auth/email/confirm', user=current_user, token=token)  # 发送email
+    flash('A new confirmation email has been sent to you by email.')
     return redirect(url_for('main.index'))
--- app/models.py
+++ app/models.py
@@ -2,6 +2,9 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -20,6 +23,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
 
     @property  # 将方法变成属性的装饰器
     def password(self):  # 如果试图读取password则返回错误
@@ -35,6 +39,22 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):  # 检查哈希密码
         return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id}).decode('utf-8')
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     def __repr__(self):
         return '<User %r>' % self.username
